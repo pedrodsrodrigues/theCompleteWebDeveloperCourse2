6a1c23af16650e6305e2d91c9cd018f2aca63cc3
Pedro Rodrigues <p.rodrigues@prodigentia.com>
2020-04-12 03:48:04 +0100

    Video 111

--- weatherScraper.php
+++ weatherScraper.php
@@ -1,3 +1,37 @@
+<?php
+// Define the var here to not give an error when the user didn't requested any city yet
+$weather  = '';
+
+$hasError = false;
+
+// Verifies if the array "_GET" has the variable "city"
+if (array_key_exists('city', $_GET)) {
+// if ($_GET['city']) {
+    $city = str_replace(' ', '-', $_GET['city']);
+
+    $url = 'http://completewebdevelopercourse.com/locations/' . $city;
+
+    $forecastPage = file_get_contents($url);
+
+    // Get the response's header
+    $file_headers = @get_headers($url);
+
+    // print_r(get_headers($url));
+
+    // When the page doesn't exist, the website sends me a blank page. So below I use that to check if it was found or not using the length
+    // if ($file_headers[0] == 'HTTP/1.1 404 Not Found'){
+    if ($file_headers[3] != 'content-length: 0') {
+        $pageArray = explode('1 &ndash; 3 Day Weather Forecast Summary:</b>', $forecastPage);
+
+        $pageArray2 = explode('</span>', $pageArray[1]);
+
+        $weather = $pageArray2[0];
+    } else {
+        $hasError = true;
+    }
+}
+?>
+
 <!DOCTYPE html>
 <html lang='en'>
 
@@ -5,13 +39,33 @@
     <meta charset='UTF-8'>
     <meta name='viewport' content='width=device-width, initial-scale=1.0'>
     <title>Weather Scraper</title>
-    <!-- <link rel='stylesheet' href='CSS.css'> -->
 </head>
 
 <body>
-    <!-- code... -->
+    <form>
+        <h1>Enter the name of a city</h1>
 
-    <!-- <script src='javascript.js'></script> -->
+        <input type='text' id='city' name='city' placeholder='London, Lisbon' value='
+            <?php
+            // Verifies if the array "_GET" has the variable "city"
+            if (array_key_exists('city', $_GET)) {
+                echo $_GET['city'];
+            }
+            ?>
+        ' required>
+
+        <input type='submit'>
+    </form>
+
+    <br>
+
+    <?php
+    if ($hasError == true) {
+        echo 'That city could not be found!';
+    } else if ($weather) {
+        echo $weather;
+    }
+    ?>
 </body>
 
 </html>
